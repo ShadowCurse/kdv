df1db1455c90bb2debacbd6cdcf405c812d28c54
ShadowCurse <48793978+ShadowCurse@users.noreply.github.com>
2025-04-21 13:03:39 +0100

    make triangle colorfull

--- shaders/simple_vert.glsl
+++ shaders/simple_vert.glsl
@@ -1,13 +1,6 @@
 #version 450
 
-// vec3 grid_planes[6] = vec3[](
-//     vec3(1, 1, 0), vec3(-1, -1, 0), vec3(-1, 1, 0),
-//     vec3(-1, -1, 0), vec3(1, 1, 0), vec3(1, -1, 0)
-// );
-//
-// void main() {
-//     gl_Position = vec4(grid_planes[gl_VertexIndex], 1.0);
-// }
+layout(location = 0) out vec3 out_color;
 
 vec2 positions[3] = vec2[](
     vec2(0.0, -0.5),
@@ -15,6 +8,13 @@ vec2 positions[3] = vec2[](
     vec2(-0.5, 0.5)
 );
 
+vec3 colors[3] = vec3[](
+    vec3(1.0, 0.0, 0.0),
+    vec3(0.0, 1.0, 0.0),
+    vec3(0.0, 0.0, 1.0)
+);
+
 void main() {
     gl_Position = vec4(positions[gl_VertexIndex], 0.0, 1.0);
+    out_color = colors[gl_VertexIndex];
 }
